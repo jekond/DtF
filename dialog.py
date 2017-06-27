09ea04dc9c568dff4ee56a7d6e1a556a03db6016
jekond <kondratenko.je@gmail.com>
2017-06-27 18:19:59 +0300

    Функции работы с текстом

---
+++ dialog.py
@@ -0,0 +1,25 @@
+def format_text(non_form_text):
+    text_list = []
+    form_text = ['','','','','']
+    start_slice = 0
+    stop_slice = '.,!?:- '
+    line_range = 15
+    now_range = 0
+    for i in range(len(non_form_text)):
+        if(non_form_text[i] in stop_slice):
+            text_list+=[non_form_text[start_slice:i+1]]
+            start_slice = i+1
+    print(text_list)
+    start_slice = 0
+
+    for i in range(len(text_list)):
+        if(now_range + len(text_list[i]) > line_range+1):
+            start_slice += 1
+            now_range = 0
+        form_text[start_slice] += text_list[i]
+        now_range += len(text_list[i])
+        #print(len(form_text[i]))
+
+    for i in range(5):
+        print(len(form_text[i]))
+    return form_text

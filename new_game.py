3e9c033adb52a74013342e2c9200ab5337708e68
jekond <kondratenko.je@gmail.com>
2017-06-27 18:19:15 +0300

    Заполнение листа персонажа

--- new_game.py
+++ new_game.py
@@ -21,10 +21,6 @@ pygame.display.set_caption("New characher")
 pos = (0,0,)#координаты клика
 
 check=0#Для проверки кликов, не проеби
-def click_on_tp(abil_or_atrib):
-        for j in range(3):
-            if(pos[0]>=test.x_tp[j] and pos[0]<=test.x_tp[j]+test.tp_w):
-                test.choise_tp(abil_or_atrib,j)
         
 def click_on_tbl(strok,Y,abil_or_atrib,tp,ab_or_at,check):#Для распределения поинтов атрибутов и абилити
     for i in range(3):
@@ -64,10 +60,6 @@ while done==False:
             elif(pos[1]<test.y_ab[-1]+15 and pos[1]>test.y_ab[0]):#брабатывает клики распределения способностей
                 click_on_tbl(6,test.y_ab,pl_ch.abilities,test.b_ab_type,test.b_ab_id,check)
 
-            elif(test.y_tp[0]<=pos[1]<=test.y_tp[0]+test.h):
-                click_on_tp(test.b_at_type)
-            elif(test.y_tp[1]<=pos[1]<=test.y_tp[1]+test.h):
-                click_on_tp(test.b_ab_type)
 
     # Flag that we are done so we exit this loop
     # ОБРАБОТКА ВСЕХ СОБЫТИЙ ДОЛЖНА НАХОДИТЬСЯ НАД ЭТИМ КОММЕНТАРИЕМ

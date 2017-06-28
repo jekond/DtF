645b6b667c7435870b2f257d515f1d0dfad4c792
jekond <kondratenko.je@gmail.com>
2017-06-28 14:36:46 +0300

    Функция разбиение текста на мелкие сообщения, дабы можно было правильно выводить его в PyGame

--- dialog.py
+++ dialog.py
@@ -1,9 +1,9 @@
 def format_text(non_form_text):
     text_list = []
-    form_text = ['','','','','']
+    form_text = ['']
     start_slice = 0
     stop_slice = '.,!?:- '
-    line_range = 15
+    line_range = 30
     now_range = 0
     for i in range(len(non_form_text)):
         if(non_form_text[i] in stop_slice):
@@ -15,11 +15,9 @@ def format_text(non_form_text):
     for i in range(len(text_list)):
         if(now_range + len(text_list[i]) > line_range+1):
             start_slice += 1
+            form_text += ['']
             now_range = 0
         form_text[start_slice] += text_list[i]
         now_range += len(text_list[i])
-        #print(len(form_text[i]))
 
-    for i in range(5):
-        print(len(form_text[i]))
     return form_text
